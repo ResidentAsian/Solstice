91e06be496376a3937772a12f7a6dc82015ffbeb
Jazer Barclay <Jazer@live.co.uk>
2013-07-13 09:20:07 +0100

    Entry point created

---
+++ src/main/Loader.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Loader {
+	/**
+	 * Entry Point
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Loader is the entry point of the application
+	}
+}
