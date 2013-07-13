0d2d5356b574a52adb4fdbdd3143c8ba66242f3a
Jazer Barclay <Jazer@live.co.uk>
2013-07-13 18:35:43 +0100

    Added another line to the loader

--- src/main/Loader.java
+++ src/main/Loader.java
@@ -8,5 +8,6 @@ public class Loader {
 	public static void main(String[] args) {
 		/* Loader is the entry point of the application */
 		System.out.println("Hello Kian");
+		System.out.println("How are you");
 	}
 }
